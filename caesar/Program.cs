f2cb13caf456e195dd917f949aa80e5b05906ecb
konradgd2005@gmail.com <konradgd2005@gmail.com>
2023-04-14 10:23:33 +0200

    szyfrowanie codu

--- caesar/Program.cs
+++ caesar/Program.cs
@@ -3,6 +3,8 @@ string clearText = "Ala ma kota, a kot ma Ale, a sierotka ma rysia";
 //zamieniamy na wielkie litery
 clearText = clearText.ToUpper();
 //iteruj przez caly tekst litera po literze
+//deklarujemy string wynikowy
+string encryptedText = "";
 foreach (char c in clearText)
 {
     //wyciagamy kod ascii danej litery
@@ -11,5 +13,8 @@ foreach (char c in clearText)
     //dodajemy wartosc klucza
     asciiCode += 3;
     char encryptedChar = (char)asciiCode;
-    Console.Write(encryptedChar);
+    //dopisujemy do szyfru
+    encryptedText += encryptedChar; 
 }
+
+Console.WriteLine("zaszyfrowany tekst: " + encryptedText);
