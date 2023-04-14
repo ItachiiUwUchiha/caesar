57d6f7316d4e2ac454ef4987241f2922de950152
konradgd2005@gmail.com <konradgd2005@gmail.com>
2023-04-14 10:23:00 +0200

    szyfrowanie codu

--- caesar/Program.cs
+++ caesar/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//zdefinuj string do zaszyfrowania
+string clearText = "Ala ma kota, a kot ma Ale, a sierotka ma rysia";
+//zamieniamy na wielkie litery
+clearText = clearText.ToUpper();
+//iteruj przez caly tekst litera po literze
+foreach (char c in clearText)
+{
+    //wyciagamy kod ascii danej litery
+    // w jezyku c# rzutowanie jawne z char na int powoduje pobranie wartosci ascii
+    int asciiCode = (int)c;
+    //dodajemy wartosc klucza
+    asciiCode += 3;
+    char encryptedChar = (char)asciiCode;
+    Console.Write(encryptedChar);
+}
